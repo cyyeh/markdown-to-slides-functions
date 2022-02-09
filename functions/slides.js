a77fa8e83713f928a6b74437c55a8eed2de70a7a
ChihYu Yeh <cyyeh@data-sci.info>
2022-02-09 16:20:43 +0800

    allow local development and update functions

--- functions/slides.js
+++ functions/slides.js
@@ -3,8 +3,14 @@ import Marpit from '@marp-team/marpit'
 const marpit = new Marpit()
 const theme = `
 /* @theme example */
+body {
+  margin: 0;
+  width: 100%;
+}
 
 section {
+  width: 100%;
+  max-height: 500px;
   background-color: #369;
   color: #fff;
   font-size: 30px;
@@ -25,14 +31,19 @@ marpit.themeSet.default = marpit.themeSet.add(theme)
 
 
 exports.handler = async event => {
-  const markdown = event.body['markdown']
+  const markdown = JSON.parse(event.body)['markdown']
+  console.log(markdown)
   const { html, css } = marpit.render(markdown)
   const htmlFile = `
   <!DOCTYPE html>
-  <html><body>
+  <html>
+  <head>
     <style>${css}</style>
+  </head>
+  <body>
     ${html}
-  </body></html>
+  </body>
+  </html>
   `
 
   return {
